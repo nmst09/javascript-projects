d01539ee12b8de0f20320604fe128136a059d8d6
your name goes here <nmst09@gmail.com>
2023-09-15 13:51:13 -0500

    Revised studio for class two.

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -11,53 +11,55 @@ let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
 let maximumMassLimit = 850000;
 let fuelTempCelsius = -225;
 let minimumFuelTemp = -300;
-let macimumFuelTemp = -150;
-let fuelLevel = '100%';
+let maximumFuelTemp = -150;
+let fuelLevel = 100;
 let weatherStatus = 'Clear';
 let preparedForLiftOff = true;
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
+let validCount = false;
 if (astronautCount <= 7){
-   preparedForLiftOff = true 
-} else {
-    preparedForLiftOff = false
+    validCount = true
 }
 // add logic below to verify all astronauts are ready
-if (astronautStatus = 'ready'){
-    preparedForLiftOff = true
-} else {
-    preparedForLiftOff = false
+let validStatus = false;
+if (astronautStatus === 'ready'){
+    validStatus = true;
 }
 // add logic below to verify the total mass does not exceed the maximum limit of 850000
-if (totalMassKg <= 850000){
-    preparedForLiftOff = true
-} else {
-    preparedForLiftOff = false
+let validMass = false;
+if (maximumMassLimit <= 850000){
+    validMass = true;
 }
 // add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
-if (fuelTempCelsius < -150 || fuelTempCelsius > -300){
-    preparedForLiftOff = false
-} else {
-    preparedForLiftOff = true
+let validTemp = false;
+if (fuelTempCelsius >= -300 && fuelTempCelsius <= -150){
+    validTemp = true;
 }
 // add logic below to verify the fuel level is at 100%
-if (fuelLevel = '100%'){
-    preparedForLiftOff = true
-} else {
-    preparedForLiftOff = false
+let validFuelLevel = false;
+if (fuelLevel = 100){
+    validFuelLevel = true;
 }
 // add logic below to verify the weather status is clear
+let safeWeather = false;
 if (weatherStatus = 'Clear'){
-    preparedForLiftOff = true
-} else {
-    preparedForLiftOff = false
+    safeWeather = true;
 }
 // Verify shuttle launch can proceed based on above conditions
-console.log('date: ' + date);
-console.log('time: ' + time);
-console.log('astronaut count: ' + astronautCount);
-console.log('crew mass: ' + crewMassKg);
-console.log('fuel mass: ' + fuelMassKg);
-console.log('shuttle mass: ' + shuttleMassKg);
-console.log('total mass: ' + totalMassKg);
-console.log('fuel temp celsius:' + fuelTempCelsius);
-console.log('weather status:' + weatherStatus);
+if (validCount && validStatus && validMass && validTemp && validFuelLevel && safeWeather){
+console.log('All systems are a go! Initiating space shuttle launch sequence.');
+console.log('---------------------------------------------------------------');
+console.log('Date: ' + date);
+console.log('Time: ' + time);
+console.log('Astronaut Count: ' + astronautCount);
+console.log('Crew Mass: ' + crewMassKg);
+console.log('Fuel Mass: ' + fuelMassKg);
+console.log('Shuttle Mass: ' + shuttleMassKg);
+console.log('Total Mass: ' + totalMassKg);
+console.log('Fuel Temp Celsius:' + fuelTempCelsius);
+console.log('Weather Status: ' + weatherStatus);
+console.log('-----------------------------------------------------------------');
+console.log('Have a safe trip astronauts!')
+} else {
+    console.log('Not safe for lift off.  One ore more conditions not met. Double check all variables.')
+}
