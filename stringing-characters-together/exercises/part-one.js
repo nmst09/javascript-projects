806c2dd14cb1517ffdeab5232ddddb7fa5ece09f
your name goes here <nmst09@gmail.com>
2023-09-16 12:45:10 -0500

    Part one of stringing characters exercises.

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -1,4 +1,4 @@
-let num = 1001;
+let num = String(1123.455);
 
 //Returns 'undefined'.
 console.log(num.length);
@@ -8,3 +8,8 @@ console.log(num.length);
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
 
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+if (String(num).includes('.')){
+    console.log(num.length-1);
+} else {
+    console.log(num.length);
+}
