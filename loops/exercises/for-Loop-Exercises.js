79c8e2264342c90a357424a42aeedaf8ffa528a5
your name goes here <nmst09@gmail.com>
2023-09-22 15:40:19 -0500

    Finished class 4 exercises.

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -4,8 +4,23 @@
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
 
+for (let a = 0; a <= 20; a++){
+  console.log(a);
+}
 
+for (let b = 1; b <= 29; b = b + 2){
+  console.log(b);
+}
 
+for (let c = 12; c >= -14; c = c - 2){
+  console.log(c);
+}
+for (let d = 50; d >= 20; d--){
+  if (d % 3 === 0){
+    console.log(d);
+  }
+}
+  
 
 /*Exercise #2: 
 Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
@@ -15,10 +30,34 @@ Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
 
+let str = "LaunchCode";
+let arr = [1, 5, 'LC101', 'blue', 42];
 
+for (let i = 0; i < arr.length; i++){
+  console.log(arr[i]);
+}
 
+let reverseStr = str.split("").reverse().join("");
+
+for (let i = 0; i < reverseStr.length; i++){
+  console.log(reverseStr[i]);
+}
 
 
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+
+  let mixedArr = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+  let evens = [], odds = [];
+
+  for (let i = 0; i < mixedArr.length; i++) {
+  let number = mixedArr[i];
+    if (number % 2 == 0) {
+      evens.push(number);
+  } else {
+    odds.push(number);
+  }
+}
+  console.log(evens);
+  console.log(odds);
--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,25 +1,44 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
-
-
-
-
+const input = require('readline-sync');
+let fuelLevel = input.question("Enter a value for the starting fuel level of the space craft: ");
+fuelLevel = Number(fuelLevel);
+let astronautsAboard = input.question("How many astronauts are aboard?: ");
+astronautsAboard = Number(astronautsAboard);
+let shuttleAltitude = 0;
 
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
 
+  while (fuelLevel <= 5000 || fuelLevel > 30000 || isNaN(fuelLevel)) {
+  fuelLevel = Number(input.question(`Sorry, ${fuelLevel} is not a valid fuel level input. Please enter a fuel level greater than 5000 and les than 30000: `));
+}
 
 
 
 
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
-  
+while (astronautsAboard < 1 || astronautsAboard > 7 || isNaN(astronautsAboard)) {
+  fuelLevel = Number(input.question(`Sorry, ${astronautsAboard} is not a valid amount of astronauts. Please enter a number between 1 and 7: `));
+}
+
+
   
   
   
 //c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
-
+while (fuelLevel-100*astronautsAboard >= 0) {
+  shuttleAltitude += 50;
+  fuelLevel -= 100*astronautsAboard;
+  }
 
 
 /*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
 
 If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”*/
+ 
+let output = console.log(`The shuttle gained an altitude of ${shuttleAltitude} km.`);
+if (shuttleAltitude >= 2000) {
+  output += console.log("Orbit acheived!");
+} else {
+  output += console.log("Failed to reach orbit.");
+}
